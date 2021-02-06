63e741b886c03b87654f6f8f4f48b04a1e911800
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-06 14:49:09 +0900

    prototypeコントローラー

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destroy]
-  before_action :move_to_index, only: [:edit]
-
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -27,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -47,11 +49,5 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-  
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 end
